0ead5ec892d328827fdf81718691f53316299b54
dag <dag@pingpong.net>
2015-04-16 22:42:03 +0200

    register sick today method

--- toggler.py
+++ toggler.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python
+
 import requests
 import ConfigParser 
+import sys
+import json
+import datetime
 
 # Load configs
 config = ConfigParser.ConfigParser()
 config.read("./conf.ini")
 api_token = config.get("ConfigSection", "api_token")
-workspace_id= config.get("ConfigSection", "workspace_id")
+workspace_id = config.get("ConfigSection", "workspace_id")
+sick_project_id = config.get("ConfigSection", "sick_project_id")
 
-
+# Prints all the projects and id's for the current workspace
 def printAllProjects():
-    r = requests.get("https://www.toggl.com/api/v8/workspaces/" + workspace_id + "/projects", auth=(api_token,"api_token"))
+    url = "https://www.toggl.com/api/v8/workspaces/" + workspace_id + "/projects" 
+    r = requests.get(url, auth=(api_token,"api_token"))
     for project in r.json():
         print "#  " + str(project["id"]) + "  #  " + project["name"]
 
-printAllProjects()
+# Registers 8 hours of sickness for today on the predefined sick-project
+def registerSickToday():
+    url = "https://www.toggl.com/api/v8/time_entries"
+    today = datetime.date.today().isoformat()
+    data = {"time_entry":{"duration":28800,"start":today+"T07:00:00.000Z","pid":sick_project_id,"created_with":"toggler.py"}}
+    r = requests.post(url, data=json.dumps(data), auth=(api_token, "api_token"))
+    print "Registered sick today."
+
+# Program entry
+commands = { "projects" : printAllProjects, "sick" : registerSickToday }
+command = sys.argv[1]
+try:
+    commands[command]()
+except KeyError:
+    print "No such command."
+
