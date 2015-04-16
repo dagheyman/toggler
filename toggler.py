2fe33de006c7e9635fb246839ba2846ea1ae3611
dag <dag@pingpong.net>
2015-04-16 21:23:09 +0200

    Now prints all projects and have config-file

--- toggler.py
+++ toggler.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 import requests
+import ConfigParser 
 
-print "toggl.."
+# Load configs
+config = ConfigParser.ConfigParser()
+config.read("./conf.ini")
+api_token = config.get("ConfigSection", "api_token")
+workspace_id= config.get("ConfigSection", "workspace_id")
 
-r = requests.get('https://www.toggl.com/api/v8/workspaces/{ID}/projects', auth=('{KEY}','api_token'))
 
-for project in r.json():
-    print project['name']
+def printAllProjects():
+    r = requests.get("https://www.toggl.com/api/v8/workspaces/" + workspace_id + "/projects", auth=(api_token,"api_token"))
+    for project in r.json():
+        print "#  " + str(project["id"]) + "  #  " + project["name"]
+
+printAllProjects()
