aa14c13cc7b2dbea7e60d623fb3571b39e11ff4d
dag <dag@pingpong.net>
2015-04-17 20:53:33 +0200

    get latest day of entry

--- toggler.py
+++ toggler.py
@@ -13,6 +13,26 @@ api_token = config.get("ConfigSection", "api_token")
 workspace_id = config.get("ConfigSection", "workspace_id")
 sick_project_id = config.get("ConfigSection", "sick_project_id")
 
+def fillUpFromToday():
+    dateOfLatestEntry = findLatestEntry()
+    print dateOfLatestEntry
+
+def findLatestEntry():
+    keepGoing = True
+    count = 0
+    while keepGoing:
+        today = datetime.date.today() - datetime.timedelta(days=count)
+        todayInIso = today.isoformat()
+        print "Will now try " + todayInIso
+        url = "https://www.toggl.com/api/v8/time_entries?start_date=" + todayInIso + "T00:00:00.000Z" + "&end_date=" + todayInIso + "T23:00:00.000Z"
+        r = requests.get(url, auth=(api_token, "api_token"))
+        if r.json():
+            print "Found an entry for " + todayInIso + ". Will stop trying now."
+            return today
+            keepGoing = False
+        count += 1
+
+
 # Prints all the projects and id's for the current workspace
 def printAllProjects():
     url = "https://www.toggl.com/api/v8/workspaces/" + workspace_id + "/projects" 
@@ -29,10 +49,10 @@ def registerSickToday():
     print "Registered sick today."
 
 # Program entry
-commands = { "projects" : printAllProjects, "sick" : registerSickToday }
-command = sys.argv[1]
+commands = { "projects" : printAllProjects, "sick" : registerSickToday, "fillup" : fillUpFromToday }
 try:
+    command = sys.argv[1]
     commands[command]()
-except KeyError:
+except (KeyError, IndexError):
     print "No such command."
 
